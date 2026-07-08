9bf45fb492d45693da1cc03cae668bad33c40a9b
AlanC <alancebohin92@gmail.com>
2026-07-08 10:24:27 +0200

    Added autentication for register and login

---
+++ app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use App\Http\Resources\UserResource;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\LoginRequest;
+
+class LoginController extends Controller
+{
+    /**
+     * Handle an incoming authentication request.
+     */
+    public function store(LoginRequest $request): array
+    {
+        $request->authenticate();
+
+        $user = $request->user();
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return [
+            "user" => new UserResource($user),
+            'token' => $token,
+        ];
+    }
+
+    /**
+     * Destroy an authenticated session.
+     */
+    public function destroy(Request $request): Response
+    {
+        $user = $request->user();
+
+        $user->currentAccessToken()->delete();
+
+        return response()->noContent();
+    }
+}
